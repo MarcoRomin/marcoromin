7ef45c98047d5079ba6b798ad5aea8477e75bd08
Marco Romin <88632822+MarcoRomin@users.noreply.github.com>
2022-02-03 17:30:02 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,8 @@
-const introTextContainer = document.getElementById("code-text");
-
-const introContainer = document.querySelector(".intro-container");
 
 const myName = "Marco Romin";
 
 String.prototype.replaceAt = function(index, replacement) {
     return this.substring(0, index) + replacement + this.substring(index + replacement.length);
-}
-
-setTimeout(function() {
-    introContainer.remove()
-}, 6000)
-// animating intro text
-
-const introDelay = 4
-
-const typingSpeed = 200
-
-const underscore = document.createElement("p")
-underscore.innerText="_"
-underscore.style.fontWeight=1000
-underscore.className = "code-letter blink"
-
-setTimeout(function () {
-    underscore.style.opacity="0";
-    underscore.style.animation="none";
-}, introDelay*1000)
-
-introTextContainer.appendChild(underscore)
-
-for(let i = 0; i < myName.length; i++) {
-    setTimeout(function() {
-    const letterElement = document.createElement("p");
-    letterElement.innerText = myName[i];
-    letterElement.style.animationDelay = `${introDelay + i/myName.length/2 - i/(typingSpeed/40)}s` // delay the animation for each letter differently
-    letterElement.className = "code-letter"
-    introTextContainer.appendChild(letterElement);
-    underscore.parentNode.appendChild(underscore)
-
-    }, i*200)
 }
 
 
@@ -56,7 +20,6 @@ let titleText = myName
 for(let i = 0; i < myName.length; i++) {
     let actualNum = i;
     if(myName[i] !== " ") titleText=titleText.replaceAt(i, randomCharacter())
-    console.log("te")
      document.title=titleText
 
     const interval = setInterval(function(){
@@ -69,7 +32,7 @@ for(let i = 0; i < myName.length; i++) {
         clearInterval(interval)
         titleText = titleText.replaceAt(actualNum, myName[actualNum])
         document.title=titleText
-    }, 2000+i*300)
+    }, 1000+i*300)
 }
 
 function scrollToVH(vh){
@@ -80,3 +43,4 @@ function scrollToVH(vh){
         behavior: 'smooth'
       });
 }
+particlesJS("particles-js", {"particles":{"number":{"value":43,"density":{"enable":true,"value_area":2500}},"color":{"value":"#ffffff"},"shape":{"type":"image","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"/assets/cog.png","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":7,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":64,"color":"#ffffff","opacity":0.1,"width":1.5},"move":{"enable":true,"speed":3,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"repulse"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":0}},"bubble":{"distance":400,"size":90,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
